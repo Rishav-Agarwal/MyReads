21fffbb4809d63905757acd64a3da35322a3bc42
Rishav Agarwal <rishavcoder@gmail.com>
2018-11-28 00:41:59 +0530

    Use API to get book data for bookshelf

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React, {Component} from 'react';
 import {Route} from 'react-router-dom';
+import * as BooksAPI from './BooksAPI';
 import './App.css';
 import ListBooks from './ListBooks';
 import SearchBooks from './SearchBooks';
 
 class BooksApp extends Component {
+
+  componentDidMount = () => {
+    BooksAPI.getAll().then(books => {
+      this.setState({books});
+    });
+  };
+
   state = {
+    books: []
   };
 
   render() {
     return (
-      <div className="app">
-        <Route exact path='/' component={ListBooks} />
+      <div className='app'>
+        <Route exact path='/' render={() => (
+          <ListBooks
+            books={this.state.books}
+          />
+        )} />
         <Route path='/search' component={SearchBooks} />
       </div>
     )
